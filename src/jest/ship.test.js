366704ab495949bf64c2491eff72ebe800aa755b
majovainilla <maria_reyes@me.com>
2020-02-07 12:01:16 -0600

    Add tests for ship factory function

--- src/jest/ship.test.js
+++ src/jest/ship.test.js
@@ -1,2 +1,28 @@
 import ship from '../js/ship';
 
+const ship1 = ship(2, 'b', 3);
+
+describe('Ship factory function', () => {
+
+  test('should have correct properties', () => {
+    expect(ship1).toMatchObject({
+      length: expect.any(Number),
+      x: 'b',
+      y: 3,
+      sinkStatus: expect.any(Boolean),
+    });
+  });
+
+  test('should return the correct properties', () => {
+    expect(ship1).toHaveProperty('length');
+  });
+
+  test('should return the correct properties', () => {
+    expect(ship1).toHaveProperty('x');
+  });
+
+  test('should return the correct properties', () => {
+    expect(ship1).not.toHaveProperty('hitArr');
+  });
+});
+
--- src/js/ship.js
+++ src/js/ship.js
@@ -1,5 +1,25 @@
-ship() {
+const ship = (length, x, y, direction) => {
+  const hitArr = [];
+  const sinkStatus = false;
 
-}
+  function position() {
+    const arr = [];
+    let cell = `${x}${y}`;
+    for (let i = 1; i = length; i += 1) {
+      if (direction = 'right') {
+        arr.push(cell);
+        cell = `${x + i}${y}`;
+      }
+    }
+  }
+
+  function hit(cell) {
+    console.log('this');
+  }
+
+  return {
+    length, x, y, sinkStatus,
+  };
+};
 
 export { ship as default };
